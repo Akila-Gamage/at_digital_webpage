6b3f35f688ccaff3a3030da37566c1059a6eb0f6
Akila Budhvin Gamage <95086925+Akila-Gamage@users.noreply.github.com>
2025-08-09 11:57:34 +0530

    Service components implemented

---
+++ components/service/ServiceCard.jsx
@@ -0,0 +1,35 @@
+export default function ServiceCard({
+  title,
+  description,
+  imgSrc,
+  reverse = false,
+  ctaLabel = "Learn More",
+  onCtaClick,
+}) {
+  return (
+    <section className="service px-5">
+      <div
+        className={
+          `d-flex flex-column flex-md-row justify-content-center align-items-center service-card ` +
+          (reverse ? "flex-md-row-reverse" : "")
+        }
+      >
+        <div className="col-12 col-md-4 d-flex justify-content-center service-img">
+          <img src={imgSrc} alt="Service image" />
+        </div>
+
+        <div className="col-12 col-md-8 service-details d-flex align-items-end flex-column">
+          <div className="service-content">
+            <h2 className="fs-2 service-title">{title}</h2>
+            <p className="fs-5 service-description">{description}</p>
+            <div className="mt-auto">
+              <button className="btn btn-primary service-btn" onClick={onCtaClick}>
+                {ctaLabel}
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
