2a8f7e0e08bfa6c90997d1856455675832dd9a84
Akila Budhvin Gamage <95086925+Akila-Gamage@users.noreply.github.com>
2025-08-09 11:55:33 +0530

    Home page implemented

---
+++ pages/Home.jsx
@@ -0,0 +1,11 @@
+import Hero from "../components/Hero";
+import Service from "../components/Service";
+
+export default function Home() {
+  return (
+    <section>
+      <Hero />
+      <Service />
+    </section>
+  );
+}
